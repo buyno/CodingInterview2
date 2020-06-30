12da76c87d212c93ece667cb6520e8111787995f
aa <pj@LAPTOP-UIRHBBQ4.localdomain>
2020-06-30 11:56:09 +0800

    mv utils to dir utils

--- utils/ListNode.h
+++ utils/ListNode.h
@@ -1,10 +1,9 @@
-struct ListNode{
+struct ListNode
+{
     int value;
     ListNode *pNext;
-
-    
 };
-void addToTail(ListNode **head,int value);
+void addToTail(ListNode **head, int value);
 void printListNode(ListNode *head);
 void printListNodeReverse(ListNode *head);
 void destroyListNode(ListNode *head);
