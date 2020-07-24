c2687c6cf0b0dbc796fcb4093f29de9edbe90b09
aa <pj@LAPTOP-UIRHBBQ4.localdomain>
2020-07-25 00:52:34 +0800

    7 v2

--- 7/main.cc
+++ 7/main.cc
@@ -1,169 +1,94 @@
 //利用中序遍历序列和前序遍历序列构造二叉树
-#include "../utils/Tree.h"
-#include <stdio.h>
+
+// 前序遍历 preorder = [3,9,20,15,7]
+// 中序遍历 inorder = [9,3,15,20,7]
+
+// 前序遍历序列的第一个值一定是root节点
+// root节点的后一个值一定是root的左子树的根节点，
+// 并且前序遍历序列中root的后面一串中，一定是前面一部分是左子树的，后面一部分是右子树的。
+// 比如说3后面的9，10是左子树的，且9是左子树的根节点，15，7是右子树的节点且15是右子树的根节点
+//
+#include <iostream>
 #include <assert.h>
+#include <vector>
+using namespace std;
 
-int findIndexInPreorder(int preorder[], int length, int value)
+struct TreeNode
 {
-    for (int i = 0; i < length; i++)
-    {
-        if (preorder[i] == value)
-        {
-            return i;
-        }
-    }
-    return -1;
-}
-void buildTree(int preorder[], int inorder[], int length, int inorderstart, int inorderend, int leftOrRight, TreeNode **head)
-{
-    int rootIndex = length;
-    int rootIndexInInroder;
-    for (int i = inorderstart; i <= inorderend; i++)
-    {
-        int index = findIndexInPreorder(preorder, length, inorder[i]);
-        if (index < rootIndex)
-        {
-            rootIndex = index;
-            rootIndexInInroder = i;
-        }
-    }
-    if (leftOrRight == 0)
-    {
-        (*head)->left = new TreeNode();
-        (*head)->left->value = preorder[rootIndex];
-        if (rootIndexInInroder > inorderstart)
-            buildTree(preorder, inorder, length, inorderstart, rootIndexInInroder - 1, 0, &((*head)->left));
-        if (rootIndexInInroder < inorderend)
-            buildTree(preorder, inorder, length, rootIndexInInroder + 1, inorderend, 1, &((*head)->left));
-    }
-    else
-    {
-        (*head)->right = new TreeNode();
-        (*head)->right->value = preorder[rootIndex];
-        if (rootIndexInInroder > inorderstart)
-        {
-            buildTree(preorder, inorder, length, inorderstart, rootIndexInInroder - 1, 0, &((*head)->right));
-        }
-        if (rootIndexInInroder < inorderend)
-        {
-            buildTree(preorder, inorder, length, rootIndexInInroder + 1, inorderend, 1, &((*head)->right));
-        }
-    }
-}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
 
-void construct(int preorder[], int length, int inorder[], TreeNode **head)
+class Solution
 {
-    if (!(*head) || (!preorder) || (!inorder))
+public:
+    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder)
     {
-        return;
-    }
-    int rootValue = preorder[0];
-    int p1 = 0;
-    for (int i = 0; i < length; i++)
-    {
-        if (inorder[i] == rootValue)
+        if (preorder.size() == 0)
         {
-            p1 = i;
-            break;
+            return nullptr;
         }
+        return buildSubTree(preorder, inorder, 0, preorder.size() - 1, 0, inorder.size() - 1);
     }
-    *head = new TreeNode();
-    (*head)->value = rootValue;
 
-    if (p1 > 0)
-        buildTree(preorder, inorder, length, 0, p1 - 1, 0, head);
-    if (p1 < length - 1)
-        buildTree(preorder, inorder, length, p1 + 1, length - 1, 1, head);
-    return;
-}
-void test1()
-{
-    int preorder[] = {1, 2, 4, 7, 3, 5, 6, 8};
-    int inorder[] = {4, 7, 2, 1, 5, 3, 8, 6};
-    TreeNode *head;
-    construct(preorder, sizeof(preorder) / sizeof(preorder[0]), inorder, &head);
-
-    assert(head->value == 1);
-    assert(head->left->value == 2);
-    assert(head->left->left->value == 4);
-    assert(head->left->left->right->value == 7);
-    assert(head->right->value == 3);
-    assert(head->right->left->value == 5);
-    assert(head->right->right->value == 6);
-    assert(head->right->right->left->value == 8);
-    assert(head->left->right == nullptr);
-    assert(head->left->left->left == nullptr);
-    assert((head->left->left->right->left == nullptr) && (head->left->left->right->right == nullptr));
-    assert(head->right->left->left == nullptr);
-    assert(head->right->left->right == nullptr);
-    assert(head->right->right->right == nullptr);
-}
-void test2()
-{
-    int *preorder = nullptr;
-    int *inorder = nullptr;
-    TreeNode *head = nullptr;
-    construct(preorder, sizeof(preorder) / sizeof(preorder[0]), inorder, &head);
-    assert(head == nullptr);
-}
-void test3()
-{
-    int preorder[] = {1, 2, 3, 4, 5};
-    int inorder[] = {1, 2, 3, 4, 5};
-    TreeNode *head;
-    construct(preorder, sizeof(preorder) / sizeof(preorder[0]), inorder, &head);
-    assert(head->value == 1);
-    assert(head->right->value == 2);
-    assert(head->right->right->value == 3);
-    assert(head->right->right->right->value == 4);
-    assert(head->right->right->right->right->value == 5);
-    assert(head->left == nullptr);
-}
-void test4()
-{
-    int preorder[] = {1, 2, 3, 4, 5};
-    int inorder[] = {5, 4, 3, 2, 1};
-    TreeNode *head;
-    construct(preorder, sizeof(preorder) / sizeof(preorder[0]), inorder, &head);
-    assert(head->value == 1);
-    assert(head->left->value == 2);
-    assert(head->left->left->value == 3);
-    assert(head->left->left->left->value == 4);
-    assert(head->left->left->left->left->value == 5);
-    assert(head->right == nullptr);
-}
-void test5()
-{
-    int preorder[] = {1};
-    int inorder[] = {1};
-    TreeNode *head;
-    construct(preorder, sizeof(preorder) / sizeof(preorder[0]), inorder, &head);
-
-    assert(head->value == 1);
-    assert(head->left == nullptr);
-    assert(head->right == nullptr);
-}
-void test6()
-{
-    int preorder[] = {1, 2, 4, 5, 3, 6, 7};
-    int inorder[] = {4, 2, 5, 1, 6, 3, 7};
-    TreeNode *head;
-    construct(preorder, sizeof(preorder) / sizeof(preorder[0]), inorder, &head);
-    assert(head->value == 1);
-    assert(head->left->value == 2);
-    assert(head->right->value == 3);
-    assert(head->left->left->value == 4);
-    assert(head->left->right->value == 5);
-    assert(head->right->left->value == 6);
-    assert(head->right->right->value == 7);
-}
+    TreeNode *buildSubTree(vector<int> &preorder, vector<int> &inorder, int preorderleft, int preorderright, int inorderleft, int inorderright)
+    {
+        if (inorderleft > inorderright)
+        {
+            return nullptr;
+        }
+        // preorder的第一个值一定是根节点
+        int rootvalue = preorder[preorderleft];
+        TreeNode *root = new TreeNode(rootvalue);
+        int leftlength = 0; //左子树的节点个数
+        while (inorder[inorderleft + leftlength] != rootvalue)
+        {
+            leftlength++;
+        }
+        //根据左子树的节点个数就可以确定递归时的边界
+        root->left = buildSubTree(preorder, inorder, preorderleft + 1, preorderleft + leftlength, inorderleft, inorderleft + leftlength - 1);
+        root->right = buildSubTree(preorder, inorder, preorderleft + leftlength + 1, preorderright, inorderleft + leftlength + 1, inorderright);
+        return root;
+    }
+};
 
 int main()
 {
-    // test1();
-    // test2();
-    test3();
-    test4();
-    test5();
-    test6();
+    {
+        Solution s;
+        vector<int> inorder = {3, 9, 20, 15, 7};
+        vector<int> preorder = {9, 3, 15, 20, 7};
+
+        auto root = s.buildTree(inorder, preorder);
+        assert(root->val == 3);
+        assert(root->left->val == 9);
+        assert(root->right->val == 20);
+        assert(root->left->left == nullptr);
+        assert(root->left->right == nullptr);
+        assert(root->right->left->val == 15);
+        assert(root->right->right->val == 7);
+        assert(root->right->left->left == nullptr);
+        assert(root->right->left->right == nullptr);
+        assert(root->right->right->left == nullptr);
+        assert(root->right->right->right == nullptr);
+    }
+    {
+        Solution s;
+        vector<int> inorder = {3};
+        vector<int> preorder = {3};
+
+        auto root = s.buildTree(inorder, preorder);
+        assert(root->val == 3);
+        assert(root->left == nullptr);
+        assert(root->right == nullptr);
+    }
+    {
+        Solution s;
+        vector<int> inorder;
+        vector<int> preorder;
+        auto root = s.buildTree(inorder, preorder);
+        assert(root == nullptr);
+    }
 }
