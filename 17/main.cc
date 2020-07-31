9df923c2dda71360c563f6bfd5ab55aa8a6efc34
aa <pj@LAPTOP-UIRHBBQ4.localdomain>
2020-07-31 18:13:53 +0800

    update 17

--- 17/main.cc
+++ 17/main.cc
@@ -1,7 +1,11 @@
 // 打印从1到最大的n位数
 
-// *int最高到9位
+// 限制：n最高到9位
 // leetcode上的题有问题，本来应该是考察大数问题的，位数也不应该有限制
+// 但是他也是有苦衷的。因为9位数，10位数以上的结果保存到vector中会消耗很大的内存(>6GB)(内存耗光之后就是：terminate called after throwing an instance of 'std::bad_alloc')
+// 如果不保存他又不能验证结果是否正确
+
+// 最后：不用vector保存，直接输出(输出结果也要一段时间)
 #include <stdio.h>
 #include <vector>
 #include <stdlib.h>
@@ -10,98 +14,34 @@
 #include <iostream>
 using namespace std;
 
-int addone(char *nums)
-{
-    int length = 0; //
-    char *mark = nums;
-    while (*mark)
-    {
-        length++;
-        mark++;
-    }
-    int bit = 0;
-    for (int i = length - 1; i >= 0; i--)
-    {
-        int tempNumber = nums[i] - '0';
-        if (i == length - 1)
-        {
-            tempNumber++;
-        }
-        tempNumber += bit;
-        bit = 0;
-        if (tempNumber >= 10)
-        {
-            bit = tempNumber / 10;
-            nums[i] = tempNumber % 10 + '0';
-            if (i == 0 && bit == 1)
-            {
-                return 0;
-            }
-        }
-        else
-        {
-            nums[i] = tempNumber + '0';
-        }
-    }
-    // printf("length:%d\n",length);
-    return 1;
-}
-
-void printNumber(char *nums)
-{
-    int start = 0;
-    char *iter = nums;
-    while (*iter == '0')
-    {
-        iter++;
-    }
-
-    printf("%s\n", iter);
-}
-
-void print(int n)
-{
-    vector<int> vec;
-    if (n <= 0)
-        return;
-    char *nums = new char[n + 1];
-    for (int i = 0; i < n; i++)
-    {
-        nums[i] = '0';
-    }
-    nums[n] = 0;
-
-    while (addone(nums))
-    {
-        vec.push_back(atoi(nums));
-        printNumber(nums);
-    }
-}
 class Solution
 {
 public:
-    vector<int> printNumbers(int n)
+    void printNumbers(int n)
     {
-        // 因为返回值限制为int，所以还是不能处理大数问题
-        vector<int> result(pow(10, n) - 1);
-        // int lastnumber = pow(10, n) - 1;
-        // for (int i = 1; i <= lastnumber; i++)
-        // {
-        //     result[i - 1] = i;
-        // }
+
         string str(n + 1, '0');
         string end(n, '9');
         end = "0" + end;
         int i = 0;
         while (str != end)
         {
-            // result[i++] = stoi(str);
             strAddOne(str);
-            // cout << str << endl;
-            result[i++] = stoi(str);
+            cout << deletezero(str) << endl;
         }
-        return result;
+        return;
     }
+    //输出的时候去掉前面的0
+    string deletezero(string &str)
+    {
+        int index = 0;
+        while (str[index] == '0')
+        {
+            index++;
+        }
+        return str.substr(index);
+    }
+    // 处理大数+1
     void strAddOne(string &str)
     {
         int bit = 1;
@@ -119,15 +59,10 @@ public:
                 bit = 0;
             }
         }
-        // cout << str << endl;
     }
 };
 int main()
 {
     Solution s;
-    auto res = s.printNumbers(2);
-    for (auto x : res)
-    {
-        cout << x << endl;
-    }
+    s.printNumbers(9);
 }
